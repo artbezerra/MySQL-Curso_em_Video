19eb504f4be5562f52a538f3116b729e4b2a957e
Arthur Santos <art.bezerra2@gmail.com>
2025-09-25 17:43:31 -0300

    preparando pastas do repo

---
+++ Aula 3 - Criando Primeiro Banco de Dados/ex001.sql
@@ -0,0 +1,11 @@
+USE cadastro;
+CREATE TABLE pessoas (
+	nome varchar(30),
+    idade tinyint (3), 
+    sexo char(1),
+    peso float,
+    altura float,
+    nacionalidade varchar(20)
+);
+
+DESCRIBE pessoas;
